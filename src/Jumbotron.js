2e40f8babcac9a66ef2c4d08a1dde20f9d9c27b7
nicolett10 <torrestapia.n@gmail.com>
2022-09-09 21:28:47 +0000

    jumbo

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,3 +1,11 @@
 export default function Jumbotron() {
-    return <div></div>
+    return (
+		<div className="jumbotron m-5">
+			<h1 className="display-4">A warm example</h1>
+			<p className="lead">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+			<a className="btn btn-primary btn-lg" href= "#" role="button">
+				Call to action!
+			</a>
+		</div>
+	);
 }
