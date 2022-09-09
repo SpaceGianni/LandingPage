8f16eb75be64e89670fddaea83d1804e61fba29e
Jime Matthies <JimeMatthies@gmail.com>
2022-09-09 20:27:24 -0300

    Navbar Actualizado

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Jumbotron() {
     return (
-		<div className="jumbotron m-5">
+		<div className="jumbotron m-5 p-5">
 			<h1 className="display-4">A warm example</h1>
 			<p className="lead">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
 			<a className="btn btn-primary btn-lg" href= "#" role="button">
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function Nabvar() {
-    return <nav className="navbar navbar-expand-lg bg-light">
+    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Start Bootstrap</a>
+            <a className="navbar-brand d-flex align-items-center" href="https://getbootstrap.com/" target="_blank">Start Bootstrap</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 dropdown-menu-end">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">About</a>
