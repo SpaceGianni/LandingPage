2a70a4405196f67ede3a274d7f6c147ccb6bbc84
Giannina Baccelliere <gbaccellierem@gmail.com>
2022-09-12 15:36:06 -0300

    cards

--- src/Components/cards.jsx
+++ src/Components/cards.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Cards = ({Cards}) => {
+const Cards = ({cards}) => {
     return (
     <div className="row mt-4">
     <div className="col-12 col-md-3 gx-4">
         <div className="card border border-0">
             <div className="card-header border border-0 rounded-0 d-flex align-items-center justify-content-center fs-3 text-secondary"
-                style="height:200px; width:100%; background-color: lightgray;">{Cards.cardHeader}</div>
+                style={"height:200px; width:100%; background-color: lightgray;"}>{cards.cardHeader}</div>
             <div className="card-body  border px-0">
-                <h5 className="card-title text-center fs-4 text text-dark">{Cards.title}</h5>
-                <p className="card-text text-center py-2 px-4"><small>{Cards.description}</small></p>
+                <h5 className="card-title text-center fs-4 text text-dark">{cards.title}</h5>
+                <p className="card-text text-center py-2 px-4"><small>{cards.description}</small></p>
             </div>
             <div className="card-footer border mb-5 d-flex justify-content-center">
-              <a href={Cards.buttonURL} className="btn btn-primary">{Cards.buttonLabel}</a>
+              <a href={Cards.buttonURL} className="btn btn-primary">{cards.buttonLabel}</a>
             </div>
         </div>
     </div> 
@@ -22,7 +22,7 @@ const Cards = ({Cards}) => {
 }
 
 Cards.propTypes = {
-Cards: PropTypes.object.isRequired
+cards: PropTypes.array.isRequired
 };
 
 
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import Footer from './Components/Footer';
-import Jumbotron from './Components/Jumbotron';
-import Navbar from './Components/Navbar';
-import Cards from './Components/Cards';
+import Footer from './components/Footer';
+import Jumbotron from './components/Jumbotron';
+import Navbar from './components/Navbar';
+import Cards from './components/Cards';
 import './App.css';
 /* import img1 from './assets/img/01.jpg' */
 
 export function Main({ jumbotron, navbar, cards, footer }) {
     return (
         <>
-            <Navbar navbar={Navbar} />
-            <Jumbotron jumbotron={Jumbotron} />
-            <Cards cards={Cards} />
-            <Footer footer={Footer} />
+            <Navbar navbar={navbar} />
+            <Jumbotron jumbotron={jumbotron} />
+            <Cards cards={cards} />
+            <Footer footer={footer} />
         </>
     )
 }
