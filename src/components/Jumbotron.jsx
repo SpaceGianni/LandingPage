341658b9f00c398135d98bc7e296b933eb577962
nicolett10 <torrestapia.n@gmail.com>
2022-09-12 20:29:27 +0000

    jumbo

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Jumbotron = ({ jumbotron }) => {
-	return (<div className="jumbotron col-md-10 bg-light mx-auto px-5 pb-5 pt-3 ">
+	return (<div className="jumbotron col-md-10 bg-light mx-auto px-5 pb-5 pt-3">
 		<h1 className="display-4">{jumbotron.title}</h1>
 		<p className="lead">{jumbotron.subtitle}</p>
 		<a className="btn btn-primary btn-lg" href={jumbotron.button.link} role="button">{jumbotron.button.label} </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 
 
 const Navbar = ({ navbar }) => {
-    return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark col-md-12 px-5 fixed-top">
+    return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark col-md-12 ps-5 fixed-top">
         <div className="container-fluid mx-5">
             <a className="navbar-brand d-flex align-items-center" href="#page-top" target="_blank">{navbar.logo}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="pe-5 collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0 dropdown-menu-end">
                     {
                         navbar.options.map(({ link, label }, index) => {
