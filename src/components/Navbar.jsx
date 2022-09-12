4bea1994ed352afb437cc85719c71ee4b852be63
Jime Matthies <JimeMatthies@gmail.com>
2022-09-12 17:53:09 -0300

    Navbar Actualizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 
 const Navbar = ({ navbar }) => {
-    return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark col-md-12 ps-5 fixed-top">
+    return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark col-sm-12 ps-5 fixed-top">
         <div className="container-fluid mx-5">
             <a className="navbar-brand d-flex align-items-center" href="#page-top" target="_blank">{navbar.logo}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
