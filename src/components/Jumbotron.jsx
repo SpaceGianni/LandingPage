dfaf4dee9f9c34b7d0d2e014398f2d9d8f6c8ae6
nicolett10 <torrestapia.n@gmail.com>
2022-09-13 13:43:34 +0000

    jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Jumbotron = ({ jumbotron }) => {
-	return (<div className="jumbotron col-md-10 bg-light mx-auto px-5 pb-5 pt-3">
-		<h1 className="display-4">{jumbotron.title}</h1>
-		<p className="lead">{jumbotron.subtitle}</p>
-		<a className="btn btn-primary btn-lg" href={jumbotron.button.link} role="button">{jumbotron.button.label} </a>
-		{/* {
-			jumbotron.options.map(({ link, label }, index) => {
-				return (
-					<li className="btn btn-primary btn-lg" role="button" key={index} href={link}>{label}</li>
-				)
-			})
-		} */}
-	</div>)
+	return (
+		<div className="container-fluid p-0 justify-content-center">
+			<div className="jumbotron col-10 m-auto p-5">
+				<h1 className="display-3">{jumbotron.title}</h1>
+				<p className="lead">{jumbotron.subtitle}</p>
+				<a className="btn btn-primary btn-lg" href={jumbotron.button.link} role="button">{jumbotron.button.label} </a>
+			</div>
+		</div>)
 
 };
 
